263b20b0a414ece1827d8c120754fe52cea8a4fc
ManI <letz0703@gmail.com>
2022-06-02 14:01:36 +0900

    habitAddForm to function: rcf

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,9 +1,11 @@
 import React, {PureComponent} from 'react';
 
-export default class HabitAddForm extends PureComponent {
-	refForm = React.createRef();
-	refInput = React.createRef();
-	onSubmit = (e) => {
+// import React, {PureComponent} from 'react';
+
+const HabitAddForm = (props) => {
+	const refForm = React.createRef();
+	const refInput = React.createRef();
+	const onSubmit = (e) => {
 		e.preventDefault();
 		const name = this.refInput.current.value;
 		name && this.props.onAdd(name);
@@ -12,18 +14,17 @@ export default class HabitAddForm extends PureComponent {
 		// todo focus ok, but error occur
 		// this.refForm.focus();
 	};
+	return (
+		<form className='form-add ' onSubmit={this.onSubmit} ref={this.refForm}>
+			<input
+				ref={this.refInput}
+				type='text'
+				className='input-add'
+				onSubmit={this.onSubmit}
+			/>
+			<button className='button-add '>Add</button>
+		</form>
+	);
+};
 
-	render() {
-		return (
-			<form className='form-add ' onSubmit={this.onSubmit} ref={this.refForm}>
-				<input
-					ref={this.refInput}
-					type='text'
-					className='input-add'
-					onSubmit={this.onSubmit}
-				/>
-				<button className='button-add '>Add</button>
-			</form>
-		);
-	}
-}
+export default HabitAddForm;
